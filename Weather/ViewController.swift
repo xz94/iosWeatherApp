c853749684d90dd9558a3ba405efa05851ae7059
Zhu.Xuan <zhuxuan@usc.edu>
2019-11-23 23:34:53 -0800

    Initial Commit

---
+++ Weather/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Weather
+//
+//  Created by Winnie Zhu on 11/23/19.
+//  Copyright © 2019 Xuan Zhu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
